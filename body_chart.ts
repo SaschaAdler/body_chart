41d2c0a15d52df19fc71d2fe59952de8bf652a39
Sascha Adler <me@sascha.ca>
2021-08-25 17:16:24 -0230

    Creates both charts, can process multiple files

--- body_chart.ts
+++ body_chart.ts
@@ -3,7 +3,6 @@ import {
   basename,
   dirname,
   fromFileUrl,
-  isAbsolute,
   join as pathJoin,
 } from "https://deno.land/std@0.106.0/path/mod.ts";
 
@@ -20,27 +19,23 @@ const COLORS: Color[] = [
   [165, 15, 21],
 ];
 
+const REVERSED_COLORS = Array.from(COLORS).reverse();
+
 /** distance: 0 to 1 */
-const interpolateColor = (distance: number) => {
+const interpolateColor = (distance: number): Color => {
   const distanceInColor = distance * (COLORS.length - 1);
   const from = COLORS[Math.floor(distanceInColor)];
   const to = COLORS[Math.ceil(distanceInColor)];
   distance = distanceInColor - Math.floor(distanceInColor);
 
-  const result: Color = from.slice() as Color;
-  for (let i = 0; i < 3; i++) {
-    result[i] = Math.round(result[i] + distance * (to[i] - from[i]));
-  }
-  return result;
+  return from.map((fromVal, i) =>
+    Math.round(fromVal + distance * (to[i] - fromVal))
+  ) as Color;
 };
 
 const rgb = (color: [number, number, number]) => {
   color = color.map((val) => Math.floor(val)) as [number, number, number];
   return `rgb(${color[0]},${color[1]},${color[2]})`;
-};
-
-const getPath = (input: string): string => {
-  return isAbsolute(input) ? input : pathJoin(Deno.cwd(), input);
 };
 
 const resourcePath = (input: string): string => {
@@ -51,98 +46,85 @@ const resourcePath = (input: string): string => {
   );
 };
 
-// TODO: drop "extreme", create two files: $X.all.png and $X.worst.png
 if (Deno.args.includes("--help")) {
-  console.log(`body_chart: generates a body chart image from a csv file
+  console.log(`body_chart: generates body chart images from CSV files
 
 USAGE:
-    body_chart [--extreme] [--output=<OUTPUT_FILE>] <INPUT_FILE>
+    body_chart <INPUT_FILE1> [<INPUT_FILE2>...]
 
-OPTIONS:
-    --extreme
-        Only uses "extreme" pain values (i.e. "2")
-    
-    --output=<OUTPUT_FILE>
-        Creates the output image file at OUTPUT_FILE.
-        By default, the output file is generated in the same directory as
-        the input file, with extension \`.png\`.`);
+The images will be found in the same directory as the files.`);
   Deno.exit(0);
 }
 
-// Parse arguments
-const parseArgs = (args: string[]) => {
-  const bare = args.filter((arg) => !arg.startsWith("--"));
-  if (bare.length === 0) {
-    console.error("Please specify an input file.");
-    Deno.exit(1);
-  } else if (bare.length > 1) {
-    console.error("Please specify only one input file.");
-    Deno.exit(Math.min(bare.length, 255));
-  }
-  const input = getPath(bare[0]);
+const BODY_IMAGE = await Deno.readTextFile(resourcePath("body.png.b64.txt"));
 
-  const threshold = args.includes("--extreme") ? 2 : 1;
-  const outputArg = args.find((arg) => arg.startsWith("--output="));
-  const output = outputArg
-    ? getPath(outputArg.slice("--output=".length))
-    : pathJoin(dirname(input), basename(input, ".csv") + ".png");
-
-  return { input, output, threshold };
-};
-
-const args = parseArgs(Deno.args);
-
-const text = await Deno.readTextFile(args.input);
-const values = (await parseCSV(text) as string[][])
-  .filter((row) =>
-    !Number.isNaN(parseInt(row[0], 10)) && !Number.isNaN(parseInt(row[1], 10))
-  )
-  .map((row) => row.slice(1).map((val) => parseInt(val, 10)));
-
-const tally = values.reduce(
-  (acc, curr) => acc.map((val, i) => curr[i] >= args.threshold ? val + 1 : val),
-  Array.from({ length: SEGMENTS }, () => 0),
-);
-
-const max = Math.max(...tally);
-
-const svg = (await Deno.readTextFile(
+const SVG_CONTENTS = (await Deno.readTextFile(
   resourcePath("chart.svg"),
-)).split("\n");
-
-for (let i = 0; i < SEGMENTS; i++) {
-  let fill: string;
-  if (tally[i] === 0) {
-    fill = `fill-opacity="0"`;
-  } else {
-    const color = interpolateColor((tally[i] - 1) / (max - 1));
-    fill = `fill="${rgb(color)}"`;
-  }
-  svg[SVG_OFFSET + i] = svg[SVG_OFFSET + i].replace(
-    `fill=""`,
-    fill,
-  );
-}
-
-const bodyImage = await Deno.readTextFile(resourcePath("body.png.b64.txt"));
-
-const tmpSvg = await Deno.makeTempFile();
-
-const svgOutput = svg
-  .join("\n")
-  .replace("$IMAGE_DATA", bodyImage)
-  .replace("$MAX", max.toString())
+))
+  .replace("$IMAGE_DATA", BODY_IMAGE)
   .replace(
     "$GRADIENT",
-    COLORS.reverse().map((color, i) =>
+    REVERSED_COLORS.map((color, i) =>
       `      <stop offset="${100 * i / (COLORS.length - 1)}%" stop-color="${
         rgb(color)
       }" stop-opacity="0.8" />`
     ).join("\n"),
-  );
+  )
+  .split("\n");
 
-await Deno.writeTextFile(tmpSvg, svgOutput);
+const inputs = Deno.args;
 
-const convertProcess = Deno.run({ cmd: ["convert", tmpSvg, args.output] });
-await convertProcess.status();
-convertProcess.close();
+for (const input of inputs) {
+  const text = await Deno.readTextFile(input);
+  const values = (await parseCSV(text) as string[][])
+    .filter((row) =>
+      !Number.isNaN(parseInt(row[0], 10)) &&
+      !Number.isNaN(parseInt(row[1], 10))
+    )
+    .map((row) => row.slice(1).map((val) => parseInt(val, 10)));
+
+  for (const type of ["any", "worst"] as const) {
+    const output = pathJoin(
+      dirname(input),
+      basename(input, ".csv") + `.${type}.png`,
+    );
+    const threshold = type === "any" ? 1 : 2;
+
+    const tally = values.reduce(
+      (acc, curr) => acc.map((val, i) => curr[i] >= threshold ? val + 1 : val),
+      Array.from({ length: SEGMENTS }, () => 0),
+    );
+
+    const max = Math.max(...tally);
+
+    const svg = Array.from(SVG_CONTENTS);
+
+    for (let i = 0; i < SEGMENTS; i++) {
+      let fill: string;
+      if (tally[i] === 0) {
+        fill = `fill-opacity="0"`;
+      } else {
+        const color = interpolateColor((tally[i] - 1) / (max - 1));
+        fill = `fill="${rgb(color)}"`;
+      }
+      svg[SVG_OFFSET + i] = svg[SVG_OFFSET + i].replace(
+        `fill=""`,
+        fill,
+      );
+    }
+
+    const tmpSvg = await Deno.makeTempFile();
+
+    const svgOutput = svg
+      .join("\n")
+      .replace("$MAX", max.toString());
+
+    await Deno.writeTextFile(tmpSvg, svgOutput);
+
+    const convertProcess = Deno.run({ cmd: ["convert", tmpSvg, output] });
+    await convertProcess.status();
+    convertProcess.close();
+
+    await Deno.remove(tmpSvg);
+  }
+}
